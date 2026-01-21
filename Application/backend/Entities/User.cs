a5ae95403993ddb1734c997b0ff2b3fd181ccce5
lukadimic <luka.dimic@rute.email>
2026-01-21 18:33:56 +0100

    reorganizacija foldera i dodavanje dokumentacije

--- Application/backend/Entities/User.cs
+++ Application/backend/Entities/User.cs
@@ -10,10 +10,10 @@ public class User {
     public string? Id { get; set; } 
     
     [BsonElement("email")]
-    public string Email { get; set; };
+    public string Email { get; set; }
     
     [BsonElement("passwordHash")]
-    public string PasswordHash { get; set; };
+    public string PasswordHash { get; set; }
     
     [BsonElement("role")]
     public Role Role { get; set; }
